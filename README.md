9ee7692168ceaaa15da784301fbbf72c19e20809
Jack Hoy <jackhoy05@gmail.com>
2015-06-08 23:03:53 +0100

    create rust function using 10 threads which count to 5 mil. make it callable from C

---
+++ README.md
@@ -0,0 +1,5 @@
+### Embedded Rust
+
+Calls Rust code from within Ruby using Foreign Function Interface (FFI). Task:
+
+>Start ten threads. Inside each thread, count from one to five million. After All ten threads are finished, print out ‘done!’.
---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::thread;
+
+// prevent change of function name in
+// compiled output
+#[no_mangle]
+
+// pub allows function to be called outside module
+// extern allows function to be called from C
+pub extern fn process () {
+    let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut _x = 0;
+            for _ in (0..5_000_001) {
+                _x += 1
+            }
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().ok().expect("Could not join a thread!");
+    }
+}
+
+#[test]
+fn test_process() {
+    process()
+}
